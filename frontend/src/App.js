9f8b8932e43d1c9847c9bfbbbcb3d23b5c13163d
Kesiya Raj <kesiya.raj@sjsu.edu>
2021-11-09 16:40:11 -0800

    HomePage

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Homepage from './components/Homepage/Homepage'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact render={props => <Homepage {...props} />} />
+        </Switch>
+      </Router>
+    </div>
+
+  );
+}
+
+export default App;
