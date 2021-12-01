c7117b156ff743f0cc3d9d2edb3f078ae9651fc9
Vasudha Pasumarthi <srivasudhavalli.pasumarthi@sjsu.edu>
2021-12-01 11:44:04 -0800

    revert seat selection changes

--- frontend/src/components/SeatSelection/SeatSelection.js
+++ frontend/src/components/SeatSelection/SeatSelection.js
@@ -78,7 +78,7 @@ export default function SeatSelection(props) {
     }
 
     const handleSkipDetails = () => {
-        props.history.push('/PaymentTab')
+                    props.history.push('/PaymentTab')
 
 
     }
