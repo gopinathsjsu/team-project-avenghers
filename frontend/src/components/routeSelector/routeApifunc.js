85bf83cb552d0c983b80211d3123bdebe437d910
NancyS1 <79034901+NancyS1@users.noreply.github.com>
2021-12-03 22:16:17 -0800

    Update routeApifunc.js

--- frontend/src/components/routeSelector/routeApifunc.js
+++ frontend/src/components/routeSelector/routeApifunc.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export async function getRoutesFromApi(startCity, destination, date) {
-    const baseURL = "http://localhost:8080/booking/"
+    const baseURL = "http://54.85.78.106:8080/booking/"
     return await axios.post(baseURL, { startCity, destination, date })
 }
